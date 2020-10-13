46935f1441d05fc171a729ca60a172b810ddd094
tomad6909 <tomad6909@wrdsb.ca>
2020-10-13 16:21:09 +0000

    finished A7Q3

--- Main.java
+++ Main.java
@@ -1,15 +1,29 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * This program will produce a pattern of a random number of asteriks between 1 and 5 along the page
+ * @author David Toma
  */
 public class Main {
 
+  // create a method
+  public static void integer(int userInput){
+    // create a for statement to determine the random number
+    for(int i = 0; i < userInput; i++){
+      int randNum = (int)(Math.random()*(5 - 1 + 1))+ 1;
+    // create a for statement to determine how many * are going to be printed
+    for(int j = 0; j < randNum; j++){
+      System.out.print("*");
+    }
+    System.out.println("");
+  } 
+}
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // input the number of lines that the user wants
+    integer(7);
     
   }
 }
